28417af14c727e24a1c07a2119b8167b86406123
Matthew Romero Moore <mjrqq3@mail.missouri.edu>
2020-02-20 10:48:05 -0600

    handles different request types. made func main() a bit more concise.

--- main.go
+++ main.go
@@ -12,18 +12,35 @@ type server struct{}
 
 // Handler interface
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// status 200; request successful
-	w.WriteHeader(http.StatusOK)
 
 	// informing client to expect JSON-formatted payload
 	w.Header().Set("Content-Type", "application/json")
 
-	// response
-	w.Write([]byte(`{"message": "hello world"}`))
+	// handle different request types
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "get called"}`))
+
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "post called"}`))
+
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message": "put called"}`))
+
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "delete called"}`))
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "not found"}`))
+	}
 }
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
+	http.Handle("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
